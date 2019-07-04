056d6c29a3144b6a398fea1c4ff5e46424187c43
Nicolas Polomack <nicolas@polomack.eu>
2019-07-04 13:54:06 +0200

    Fixed issue when publishing new crates

--- src/api.rs
+++ src/api.rs
@@ -5,6 +5,7 @@ use std::sync::{Arc, Mutex};
 
 use byteorder::{LittleEndian, ReadBytesExt};
 use diesel::prelude::*;
+use diesel::result::Error as SQLError;
 use rocket::{Data, State};
 use rocket_contrib::json::Json;
 use semver::{Version, VersionReq};
@@ -13,7 +14,9 @@ use sha2::{Digest, Sha256};
 
 use crate::db::models::{CrateRegistration, ModifyCrateRegistration, NewCrateRegistration};
 use crate::db::schema::*;
-use crate::{AlexError, AppState, Auth, Crate, DbConn, Dependency, DependencyKind, Error, Indexer, Store};
+use crate::{
+    AlexError, AppState, Auth, Crate, DbConn, Dependency, DependencyKind, Error, Indexer, Store,
+};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct APIPublishResponse {}
@@ -76,64 +79,81 @@ pub fn api_publish(
     let mut metadata_bytes = vec![0u8; metadata_size as usize];
     stream.read_exact(&mut metadata_bytes)?;
     let metadata: APICrateMeta = json::from_slice(&metadata_bytes)?;
-    let krate: CrateRegistration = crates::table
-        .filter(crates::name.eq(metadata.name.as_str()))
-        .first(&conn.0)?;
     let crate_size = stream.read_u32::<LittleEndian>()?;
     let mut crate_bytes = vec![0u8; crate_size as usize];
     stream.read_exact(&mut crate_bytes)?;
     let hash = String::from_utf8(Sha256::digest(&crate_bytes).to_vec()).unwrap();
 
     let state = state.lock().unwrap();
-    match state.index().max_version(krate.name.as_str()) {
-        Ok(max_version) if metadata.vers <= max_version => Err(Error::from(
-            AlexError::VersionTooLow(krate.name, max_version, metadata.vers),
-        )),
-        Ok(_) => {
-            state.storage().store_crate(&metadata.name, metadata.vers.clone(), crate_bytes.as_slice())?;
+    let krate: Result<CrateRegistration, SQLError> = crates::table
+        .filter(crates::name.eq(metadata.name.as_str()))
+        .first(&conn.0);
+    match krate {
+        Ok(krate) => {
+            let max_version = state.index().max_version(krate.name.as_str())?;
+            if metadata.vers <= max_version {
+                Err(Error::from(AlexError::VersionTooLow(
+                    krate.name,
+                    max_version,
+                    metadata.vers,
+                )))
+            } else {
+                state.storage().store_crate(
+                    &metadata.name,
+                    metadata.vers.clone(),
+                    crate_bytes.as_slice(),
+                )?;
 
-            let path = state.index().index_crate(&metadata.name);
-            let crate_desc = Crate {
-                name: metadata.name,
-                vers: metadata.vers,
-                deps: metadata
-                    .deps
-                    .into_iter()
-                    .map(|dep| Dependency {
-                        name: dep.name,
-                        req: dep.version_req,
-                        features: dep.features,
-                        optional: dep.optional,
-                        default_features: dep.default_features,
-                        target: dep.target,
-                        kind: dep.kind,
-                    })
-                    .collect(),
-                cksum: hash,
-                features: metadata.features,
-                yanked: Some(false),
-            };
-            let mut file = fs::OpenOptions::new().write(true).append(true).open(path)?;
-            json::to_writer(&mut file, &crate_desc)?;
-            write!(file, "\n")?;
-            file.flush()?;
-            state.index().commit_and_push(&format!("Updating crate `{}#{}`", &crate_desc.name, &crate_desc.vers))?;
+                let path = state.index().index_crate(&metadata.name);
+                let crate_desc = Crate {
+                    name: metadata.name,
+                    vers: metadata.vers,
+                    deps: metadata
+                        .deps
+                        .into_iter()
+                        .map(|dep| Dependency {
+                            name: dep.name,
+                            req: dep.version_req,
+                            features: dep.features,
+                            optional: dep.optional,
+                            default_features: dep.default_features,
+                            target: dep.target,
+                            kind: dep.kind,
+                        })
+                        .collect(),
+                    cksum: hash,
+                    features: metadata.features,
+                    yanked: Some(false),
+                };
+                let mut file = fs::OpenOptions::new().write(true).append(true).open(path)?;
+                json::to_writer(&mut file, &crate_desc)?;
+                write!(file, "\n")?;
+                file.flush()?;
+                state.index().commit_and_push(&format!(
+                    "Updating crate `{}#{}`",
+                    &crate_desc.name, &crate_desc.vers
+                ))?;
 
-            let new_crate = ModifyCrateRegistration {
-                id: krate.id,
-                name: crate_desc.name.as_str(),
-                description: metadata.description.as_ref().map(|s| s.as_str()),
-                documentation: metadata.documentation.as_ref().map(|s| s.as_str()),
-                repository: metadata.repository.as_ref().map(|s| s.as_str()),
-            };
-            diesel::update(crates::table)
-                .set(new_crate)
-                .execute(&conn.0)?;
+                let new_crate = ModifyCrateRegistration {
+                    id: krate.id,
+                    name: crate_desc.name.as_str(),
+                    description: metadata.description.as_ref().map(|s| s.as_str()),
+                    documentation: metadata.documentation.as_ref().map(|s| s.as_str()),
+                    repository: metadata.repository.as_ref().map(|s| s.as_str()),
+                };
+                diesel::update(crates::table)
+                    .set(new_crate)
+                    .execute(&conn.0)?;
 
-            Ok(Json(APIPublishResponse {}))
+                Ok(Json(APIPublishResponse {}))
+            }
         }
-        Err(Error::AlexError(AlexError::CrateNotFound(_))) => {
-            state.storage().store_crate(&metadata.name, metadata.vers.clone(), crate_bytes.as_slice())?;
+        Err(SQLError::NotFound) => {
+            state.storage().store_crate(
+                &metadata.name,
+                metadata.vers.clone(),
+                crate_bytes.as_slice(),
+            )?;
 
             let path = state.index().index_crate(metadata.name.as_str());
             let crate_desc = Crate {
@@ -163,7 +183,10 @@ pub fn api_publish(
             json::to_writer(&mut file, &crate_desc)?;
             write!(file, "\n")?;
             file.flush()?;
-            state.index().commit_and_push(&format!("Adding crate `{}#{}`", &crate_desc.name, &crate_desc.vers))?;
+            state.index().commit_and_push(&format!(
+                "Adding crate `{}#{}`",
+                &crate_desc.name, &crate_desc.vers
+            ))?;
 
             let new_crate = NewCrateRegistration {
                 name: crate_desc.name.as_str(),
@@ -176,7 +199,7 @@ pub fn api_publish(
                 .execute(&conn.0)?;
             Ok(Json(APIPublishResponse {}))
         }
-        Err(err) => Err(err),
+        Err(err) => Err(Error::from(err)),
     }
 }
 
