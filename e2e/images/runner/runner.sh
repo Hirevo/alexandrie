5cc61fe8b85626c3c8f983267e980702746de92a
Nicolas Polomack <nicolas@polomack.eu>
2020-04-28 04:07:41 +0200

    Fixed issue with SSH key permissions in E2E tests

--- e2e/images/runner/runner.sh
+++ e2e/images/runner/runner.sh
@@ -35,12 +35,15 @@ function ssh_setup_error {
 
 trap 'ssh_setup_error' ERR
 
-# # create `${HOME}/.ssh/` directory
-# mkdir -p "${HOME}/.ssh"
+# create `${HOME}/.ssh/` directory
+mkdir -p "${HOME}/.ssh"
 
-# # copy private key to that `${HOME}/.ssh/` folder
-# cp "/run/secrets/git_ssh_key" "${HOME}/.ssh/id_rsa"
-# chmod 400 "${HOME}/.ssh/id_rsa"
+# copy private key to that `${HOME}/.ssh/` folder
+cp "/run/secrets/git_ssh_key" "${HOME}/.ssh/id_rsa"
+
+# set the appropriate permissions to both the folder and the key
+chmod 700 "${HOME}/.ssh" # execute permission for folders is needed to access it
+chmod 600 "${HOME}/.ssh/id_rsa"
 
 # fetch public key of the crate index
 ssh-keyscan -t rsa index >> "${HOME}/.ssh/known_hosts"
