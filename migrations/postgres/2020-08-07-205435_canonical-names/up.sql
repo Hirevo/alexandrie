ec270a5882f428c57950917fe915c0d3dbeca199
Nicolas Polomack <nicolas@polomack.eu>
2020-08-08 14:34:08 +0200

    Fixed issue with PostgreSQL migration
    Fixed other CI issues related to PostgreSQL

--- migrations/postgres/2020-08-07-205435_canonical-names/up.sql
+++ migrations/postgres/2020-08-07-205435_canonical-names/up.sql
@@ -1,4 +1,4 @@
 alter table crates add column canon_name varchar(255);
-update crates set canon_name = replace(name, "-", "_");
-alter table crates modify canon_name varchar(255) not null;
+update crates set canon_name = replace(name, '-', '_');
+alter table crates alter column canon_name set not null;
 alter table crates add unique (canon_name);
