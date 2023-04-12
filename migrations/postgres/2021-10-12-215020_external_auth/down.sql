31e91995ffb1d6c5bf9adef89a9af2a0fa5e7657
Nicolas Polomack <nicolas@polomack.eu>
2023-04-12 22:51:45 +0200

    fix: fixed constraint bug in postgres migration

--- migrations/postgres/2021-10-12-215020_external_auth/down.sql
+++ migrations/postgres/2021-10-12-215020_external_auth/down.sql
@@ -1,3 +1,4 @@
 alter table "authors" drop column "github_id";
 alter table "authors" drop column "gitlab_id";
 alter table "authors" alter column "passwd" varchar(255) unique;
+alter table "authors" alter column "passwd" set not null;
--- migrations/postgres/2021-10-12-215020_external_auth/up.sql
+++ migrations/postgres/2021-10-12-215020_external_auth/up.sql
@@ -1,3 +1,4 @@
 alter table "authors" add column "github_id" varchar(255) unique;
 alter table "authors" add column "gitlab_id" varchar(255) unique;
 alter table "authors" alter column "passwd" type varchar(255);
+alter table "authors" alter column "passwd" drop not null;
