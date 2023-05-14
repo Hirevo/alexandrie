56136dc5a8741791811726cda8c96587b4fdb912
Dalvany <9901407+Dalvany@users.noreply.github.com>
2023-05-14 23:49:39 +0200

    Fix clippy warning
    (cherry picked from commit 217fc6892c376c82e33c1d45c62ed529905c6ec3)

--- crates/alexandrie/src/frontend/search.rs
+++ crates/alexandrie/src/frontend/search.rs
@@ -41,7 +41,8 @@ pub(crate) async fn get(req: Request<State>) -> tide::Result {
     let repo = &state.db;
 
     let (count, results) = {
-        let tantivy = (&state.search)
+        let tantivy = state
+            .search
             .read()
             .map_err(|error| Error::PoisonedError(error.to_string()))?;
         tantivy.search(
